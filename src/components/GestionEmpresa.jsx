d9043dfceb7b6e26343a37c293ea3497049ad81e
Mhgrs <felipesotosanhueza@gmail.com>
2026-03-30 09:41:39 -0300

    gestion empresa

--- src/components/GestionEmpresa.jsx
+++ src/components/GestionEmpresa.jsx
@@ -100,7 +100,7 @@ function TabEquipo() {
           <table className="min-w-full divide-y divide-gray-200">
             <thead className="bg-gray-50">
               <tr>
-                <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Empleado</th>
+                <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Usuario</th>
                 <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Roles Asignados</th>
               </tr>
             </thead>
